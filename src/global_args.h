d1db41c28537e6f99204f86f096c0d4b3d600951
Greg Steinbrecher <steinbrecher@alum.mit.edu>
2013-11-18 11:26:18 -0500

    Changed command line interface to be more sensible

--- src/global_args.h
+++ src/global_args.h
@@ -3,30 +3,27 @@
 
 struct global_args_t {
   double bin_time; // '-b' option
-  double correlation_window; // '-c' option
-  double rate_window; // '-r' option
+  double correlation_window; // '-w' option
+  
+  int channel_pairs; // Calculated as choose(channels, 2);
 } global_args;
 
 static const struct option longopts[] = {
   { "bin-time", required_argument, NULL, 'b'},
-  { "corr-window", required_argument, NULL, 'c'},
-  { "rate-window", required_argument, NULL, 'r'},
+  { "correlation-window", required_argument, NULL, 'w'},
 };
 
-
-static const char *optstring = "b:c:r:";
+static const char *optstring = "b:r:w:";
 
 int read_cli(int argc, char* argv[]) 
 {
   // Default values
   global_args.bin_time = 10*1000;
   global_args.correlation_window = 100*1000;
-  global_args.rate_window = 1e10;
 
-  if((argc % 2) != 0)
-    {
-      printf("Usage: read_hh -b [bin_time] -c [corr_window] infile\n");
-      printf("infile is a HydraHarp ht2 or ht3 file (binary)\n"); 
+  if(((argc % 2) != 0) || (argc == 0)) {
+      printf("Usage: read_hh [options] infile\n");
+      printf("infile is a HydraHarp .ht2 or .ht3 file (binary)\n"); 
       //getch();
       return(-1);
     }
@@ -42,11 +39,8 @@ int read_cli(int argc, char* argv[])
 	case 'b':
 	  global_args.bin_time = atof(optarg)*1e3;
 	  break;
-	case 'c':
+	case 'w':
 	  global_args.correlation_window = atof(optarg)*1e3;
-	  break;
-	case 'r':
-	  global_args.rate_window = atof(optarg)*1e3;
 	  break;
 	default: 
 	  // Shouldn't actually get here
