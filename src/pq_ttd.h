b76eb1f3880dfb21e654828c57248b8aaa334a1b
Greg Steinbrecher <steinbrecher@alum.mit.edu>
2014-01-15 18:32:57 -0500

    Updated pq_ttd interface file to include all conversion functions

--- src/pq_ttd.h
+++ src/pq_ttd.h
@@ -3,17 +3,16 @@
 
 #define PHOTONBLOCK 32768
 
+int pt2_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, ttd_t *overflow_correction, pq_fileinfo_t *file_info);
+int pt3_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, ttd_t *overflow_correction, pq_fileinfo_t *file_info);
+int ht2_v1_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
+int ht2_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
+int ht3_v1_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
+int ht3_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
+
 int read_header(FILE *fpin);
 
 void write_convert_propeties();
-
-int ht2_v1_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
-
-int ht2_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
-
-int ht3_v1_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
-
-int ht3_v2_to_ttd(pq_rec_t pq_rec, ttd_t *ttd_rec, uint64_t *overflow_correction, pq_fileinfo_t *file_info);
 
 uint64_t run_hh_convert(FILE *fpin, pq_fileinfo_t *file_info);
 
