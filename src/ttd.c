989e6284f5f8effe009fa4b3709cd59e34ff62b7
Greg Steinbrecher <steinbrecher@alum.mit.edu>
2014-01-15 17:47:53 -0500

    Use long double division for ttd_t rounding
    This is probably not ideal; there's definitely a better way to
    do the rounding efficiently without the cast. But for now this
    *definitely* produces the correct behavior, where other methods
    were definitely not. So I'm sticking with this for now, and will
    debug the other behavior later.

--- src/ttd.c
+++ src/ttd.c
@@ -10,21 +10,17 @@
 #include "ttd.h"
 
 ttd_t ttd_rounded_divide(ttd_t t1, ttd_t t2) {
-  return (t1 + t2 / 2) / t2;
+  long double t1d = t1;
+  long double t2d = t2;
+  
+  return (ttd_t)llrintl(t1d/t2d);
 }
 
 int64_t int64_rounded_divide(int64_t t1, int64_t t2) {
-  return (t1 + t2 / 2) / t2;
-}
-
-
-// This method is faster, but is going to be inaccurate for large values 
-// of t1 or t2 due to limited floating point precision. This function 
-// deliberately has the same interface as ttd_rounded_divide so that a 
-// conditional function pointer may be set depending on which is desired
-// at a given part of program execution.
-ttd_t ttd_rounded_divide_unsafe(ttd_t t1, ttd_t t2) {
-  return (ttd_t) round( ((double)t1) / ((double)t2) );
+  long double t1d = t1;
+  long double t2d = t2;
+  
+  return llrintl(t1d/t2d);
 }
 
 void ttd_print_version() {
