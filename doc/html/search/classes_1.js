0ad848ee0344399797141b51c02336b87e9ea4f8
Greg Steinbrecher <steinbrecher@alum.mit.edu>
2016-04-13 20:22:55 -0400

    Removing html documentation from main branch

--- doc/html/search/classes_1.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['tghd',['TgHd',['../struct_tg_hd.html',1,'']]],
-  ['ttd_5fccorr2_5ft',['ttd_ccorr2_t',['../structttd__ccorr2__t.html',1,'']]],
-  ['ttd_5fccorr3_5ft',['ttd_ccorr3_t',['../structttd__ccorr3__t.html',1,'']]],
-  ['ttd_5fccorr4_5ft',['ttd_ccorr4_t',['../structttd__ccorr4__t.html',1,'']]],
-  ['ttd_5ffb_5ft',['ttd_fb_t',['../structttd__fb__t.html',1,'']]],
-  ['ttd_5frb_5ft',['ttd_rb_t',['../structttd__rb__t.html',1,'']]]
-];
--- doc/html/search/variables_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['window_5ftime',['window_time',['../structttd__ccorr2__t.html#a2afa4d3a622ea10c731181ecaefc63c3',1,'ttd_ccorr2_t']]]
-];
